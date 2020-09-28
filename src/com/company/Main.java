c469d5997b78dfe721ba1fd614da29b8f7fd4778
34 <34>
2020-09-28 15:46:33 -0200

    1 задача (рандомные числа)

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,12 @@
 package com.company;
 
+import static java.lang.Math.round;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        long[] mass = new long[100];
+        for (int i = 0; i < mass.length; i++) { mass[i] = round(Math.random() * 100);}
+        for (long i: mass) {System.out.print(i + " ");}
     }
 }
